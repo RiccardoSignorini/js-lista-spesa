2933ecd0d871e0de5f0fd9fe1c9afdfda3b0f7a0
RiccardoSignorini <riccardo.signorini1@gmail.com>
2023-03-30 09:28:29 +0200

    preliminare svolto con for

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,20 @@
 --PROBLEMA--
 Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
 */
+
+const arrayList = [
+    `pomodori`,
+    `insalata`,
+    `mele`,
+    `avocado`,
+    `uova`,
+    `latte`,
+]
+
+for( let i=0; i<arrayList.length; i++ ){
+    const shoppingList = listArray[i];
+    console.log(shoppingList)
+}
+
+
+
